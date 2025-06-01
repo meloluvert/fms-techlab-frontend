a87ef4f8a91eb40e65692f46b06691b9cfbdcb1d
lucas <lucasmelo8328@gmail.com>
2025-05-31 23:24:41 -0300

    fix: aumentando tamanho da animação de login

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -4,7 +4,7 @@ import loadingAnimation from "../../assets/loading.json";
 export function Loading() {
   return (
     <div className="flex justify-center items-center pt-4">
-      <Lottie animationData={loadingAnimation} loop={true} style={{ width: "60%", height: "auto" }} />
+      <Lottie animationData={loadingAnimation} loop={true} style={{ width: "60vw", height: "auto" }} />
     </div>
   );
 }
